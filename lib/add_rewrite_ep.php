48cbf83acafab78b8b3f52da8de776f6f90a59bf
miya <miya@wpist.me>
2013-05-14 23:55:53 +0900

    first commit

---
+++ lib/add_rewrite_ep.php
@@ -0,0 +1,47 @@
+<?php
+
+if (!class_exists('Add_Rewrite_EP')) :
+
+class Add_Rewrite_EP {
+
+private $ep;
+private $mask;
+
+function __construct($ep, $callback, $mask = EP_ROOT)
+{
+    $this->ep = urlencode($ep);
+    $this->callback = $callback;
+    $this->mask = $mask;
+}
+
+public function register()
+{
+    add_action('init', array(&$this, 'init'));
+    add_action('query_vars', array(&$this, 'query_vars'));
+    add_action('template_redirect', array(&$this, 'template_redirect'));
+    add_action('ep_action_'.$this->ep, $this->callback);
+}
+
+public function init()
+{
+    add_rewrite_endpoint($this->ep, $this->mask);
+}
+
+public function query_vars($vars)
+{
+    $vars[] = $this->ep;
+    return $vars;
+}
+
+public function template_redirect()
+{
+    global $wp_query;
+    if (isset($wp_query->query[$this->ep])) {
+        do_action('ep_action_'.$this->ep, get_query_var($this->ep));
+    }
+}
+
+} // end ADD_EP_ROOT
+endif;
+
+// EOF
